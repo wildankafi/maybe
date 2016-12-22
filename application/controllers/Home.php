77b81afd67b190dabd7e41f2cc76ff5b220ededc
wildan mukafi <wildankafie@yahoo.co.id>
2016-12-23 02:38:24 +0700

    home controller

---
+++ application/controllers/Home.php
@@ -0,0 +1,34 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+ 
+class Home extends CI_Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->helper('text');
+        $this->load->model('Model_app');
+        $data['query'] = $this->Model_app->getAll_datatips();
+    }
+ 
+ 
+    /**
+     * Cotoh penggunaan bootstrap pada codeigniter::index()
+     */
+    public function index()
+    {
+    $this->load->helper('text');
+    $this->load->model('Model_app');
+    $data['query'] = $this->Model_app->getAll_datatips();
+    $this->load->view('displayhome', $data);
+    }
+
+    function masuk_admin(){
+        $this->load->view('homeAdmin');
+    }
+    public function selanjutnya()
+    {
+        $id=$this->uri->segment(3);
+        $data['query']=$this->Model_app->per_id($id);
+        $this->load->view('displaydetail',$data);
+    }
+}
